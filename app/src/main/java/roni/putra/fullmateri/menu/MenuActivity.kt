5342a3e25bc59f8990d1841445bcfd0edfb324ca
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-08-23 10:39:58 +0700

    Update Code

---
+++ app/src/main/java/roni/putra/fullmateri/menu/MenuActivity.kt
@@ -0,0 +1,38 @@
+package roni.putra.fullmateri.menu
+
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import roni.putra.fullmateri.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.top_logout -> {
+                Toast.makeText(applicationContext, "click logout", Toast.LENGTH_LONG).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
+
